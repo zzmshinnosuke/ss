fe6dbba67135fc2e67e6a407257c7fc1b3739910
zzmshinnosuke <1608295563@qq.com>
2019-06-04 17:11:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 yum install python-setuptools && easy_install pip
 pip install shadowsocks
 
+wget https://raw.githubusercontent.com/zzmshinnosuke/ss/master/shadowsocks.json
 
+ssserver -c shadowsocks.json -d start
